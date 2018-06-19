931654fea2e5845165877a27348da0c54c0ac38d
Yuri Alves de Almeida <yurialves2@gmail.com>
2018-06-19 01:15:34 -0300

    Resolvendo dependências automaticamente

--- index.php
+++ index.php
@@ -3,15 +3,28 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Yuri\Router\Router;
+use Yuri\DI\Resolver;
 
 $path_info = $_SERVER['PATH_INFO'] ?? '/';
 $request_method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
 
 $router = new Router($path_info, $request_method);
 
-$router->get('/hello/{name}', function($params) {
-    return 'Meu nome é ' . $params[1];
-});
+class User
+{
+    public function __construct($name = 'fulano')
+    {
+        echo $name;
+    }
+}
+
+require 'router.php';
 
 $result = $router->run();
-print_r($result['callback']($result['params']));
+
+$data = (new Resolver)->method($result['callback'], [
+    'params' => $result['params']
+]);
+// print_r($result['callback']($result['params']));
+
+var_dump($data);
